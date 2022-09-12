9a6d06c9fd5eb5b09a0bbf49c460d2c9e17f75b4
Sushmita262 <43111026+Sushmita262@users.noreply.github.com>
2022-09-12 23:38:52 +0600

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,11 @@
+Val1 = 20180
+Val2 = 118
+Val3 = 1999
+Sumation=Val1+Val2 
+print(f"Sumation of {Val1} and {Val2} is {Sumation}")
+print(type(Sumation))
+Subtraction=Val3-Val2
+print(f"Subtraction of {Val3} and {Val2} is {Subtraction}")
+Multiplication = Subtraction * Val2
+print(f"Multiplication of {Subtraction} and {Val2} is {Multiplication}")
+print(type(Multiplication))
